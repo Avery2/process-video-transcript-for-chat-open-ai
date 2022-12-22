9b07e11113f3f5a2c1c18978c0d088b2d320198a
Avery2 <justinaverychan@gmail.com>
2022-12-22 03:17:42 -0800

    feat: add parameter to disable sections

--- process_video_transcript.py
+++ process_video_transcript.py
@@ -1,6 +1,7 @@
 import re
 
 DISALLOWED_CHARACTERS = set(['-', '*', ''])
+DISABLE_TIMESTAMP = False
 
 def isTimestamp(s):
   timestamp_regex = r'\d{1,2}:{1,2}'
@@ -26,6 +27,8 @@ with open('timestamps.txt') as f:
   for line in lines:
     timestamp, title = line.split(' ')[0], ' '.join(list(filter(lambda x: x.strip() not in DISALLOWED_CHARACTERS, line.split(' ')[1:]))).strip()
     timestamped_titles.append((timestamp, title))
+if DISABLE_TIMESTAMP:
+  timestamped_titles = ['00:00', 'Default Title']
 
 timestamped_titles.sort(key=lambda x: x[0], reverse=True)
 
